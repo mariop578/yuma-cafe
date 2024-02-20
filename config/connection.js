51e4b4463f93f1836ad5a84c5ab1bf8481207988
mariop <partidam578@gmail.com>
2024-02-20 01:17:13 -0700

    Added JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASWWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASWWORD,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
